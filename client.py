fc024fbf1403f20f5b5dccf6993e5dade1a18f58
Jay Ngo <you@example.com>
2020-06-10 15:48:21 +0700

    first commit

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+serverName  = 'localhost'
+serverPort = 12000
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = raw_input('Input lowercase sentence: ')
+clientSocket.send(sentence)
+modifiedSentence = clientSocket.recv(1024)
+print ('From server: ', modifiedSentence)
+clientSocket.close()
